284547f6e8a7d299c1ab4376f958802cc37c223f
indrek-png <indrek.noor@tktk.ee>
2026-05-17 10:38:47 +0300

    Add function to append task to a file

---
+++ add_to_file.py
@@ -0,0 +1,8 @@
+def add_task_to_file(file_path):
+
+    new_task = "Clebrate small wins with a cookie."
+
+    with open(file_path, "a", encoding="utf-8") as f:
+        f.write("\n" + new_task)
+
+add_task_to_file(r"data\todo.txt")
